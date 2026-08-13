4dd167433b333832ef62133f76fb3167184323d5
farah <farah.hamdii55@gmail.com>
2026-08-14 01:44:11 +0300

    docs: add screenshots and final project updates

--- phase-01-backend-foundations/task-01-csharp-drills/Task-01-csharp-drills/Task-01-csharp-drills/Program.cs
+++ phase-01-backend-foundations/task-01-csharp-drills/Task-01-csharp-drills/Task-01-csharp-drills/Program.cs
@@ -8,7 +8,7 @@ namespace Task_01_csharp_drills
         static void Main(string[] args)
         {
             // Drill01_TemperatureConverter.Run();
-            // Drill02_GradeCalculator.Run();
+            //Drill02_GradeCalculator.Run();
             //Drill03_LoginValidator.Run();
             // Drill04_EvenOddAnalyzer.Run();
             //Drill05_MaxMinFinder.Run();
@@ -16,17 +16,17 @@ namespace Task_01_csharp_drills
             // Drill07_NameFormatter.Run();
             //Drill08_PasswordStrengthChecker.Run();
             //Drill09_ShoppingCartTotal.Run();
-            //Drill10_SimpleAtmMenu.Run();
+           // Drill10_SimpleAtmMenu.Run();
             // Drill11_DuplicateNumberDetector.Run();
             // Drill12_EmailValidator.Run();
             //Drill13_PalindromeChecker.Run();
             // Drill14_ExpenseTracker.Run();
             //Drill15_ArrayRotation.Run();
-            //Drill16_FrequencyCounter.Run();
+            Drill16_FrequencyCounter.Run();
             //Drill17_SimpleSearchEngine.Run();
             //Drill18_NumberStatistics.Run();
             //Drill19_TicketPriceCalculator.Run();
-            Drill20_MethodRefactoringChallenge.Run();
+            //Drill20_MethodRefactoringChallenge.Run();
         }
     }
 }
